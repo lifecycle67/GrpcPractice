9c17012bdeb3018e39bf89ffdcbe03791c6a86c5
hsh <hsh@innodep.com>
2022-02-04 01:31:22 +0900

    서버 스트리밍 예제 코드 작성

--- GrpcPracticeClient/MainWindow.xaml.cs
+++ GrpcPracticeClient/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using Protos.Greet;
+using Protos.Streaming;
 using System.Windows;
 using static Protos.Greet.Greeter;
+using static Protos.Streaming.StreamingService;
 
 namespace GrpcPracticeClient
 {
@@ -10,13 +13,17 @@ namespace GrpcPracticeClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        GrpcChannel _channel;
+
         public MainWindow()
         {
             InitializeComponent();
+            _channel = GrpcChannel.ForAddress("http://localhost:5000");
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GreeterClient client = new GreeterClient(GrpcChannel.ForAddress("http://localhost:5000"));
+            GreeterClient client = new GreeterClient(_channel);
 
             var response = client.SayHello(
                 new HelloRequest
@@ -25,6 +32,22 @@ namespace GrpcPracticeClient
                 });
 
             MessageBox.Show(response.Message);
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var client = new StreamingServiceClient(_channel);
+
+            using var call = client.ServerStreaming(
+                new ServerStreamRequest
+                {
+                    Name = name.Text
+                });
+
+            await foreach (var response in call.ResponseStream.ReadAllAsync())
+            {
+                Responses.Items.Add(response.Message);
+            }
         }
     }
 }
---
+++ GrpcPracticeServer/Services/ServerStreamingService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Protos.Streaming;
+using static Protos.Streaming.StreamingService;
+
+namespace GrpcPracticeServer.Services
+{
+    public class ServerStreamingService : StreamingServiceBase
+    {
+        public ServerStreamingService() { }
+
+        public override async Task ServerStreaming(
+            ServerStreamRequest request, 
+            IServerStreamWriter<ServerStreamResponse> responseStream, 
+            ServerCallContext context)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string message = $"{request.Name} - response time : {DateTime.Now}";
+                Console.WriteLine(message);
+
+                await responseStream.WriteAsync(
+                    new ServerStreamResponse
+                    {
+                        Message = message
+                    });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
